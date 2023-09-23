563c0a7c16a5874a5102a3a3f9edb8a41045c7e5
karl-police <karl-police2001@bluewin.ch>
2023-09-23 17:09:38 +0200

    Update CS2Patch.hpp

--- CS2Downloader/CS2Patch.hpp
+++ CS2Downloader/CS2Patch.hpp
@@ -6,6 +6,6 @@ namespace Patcher {
 	bool ReplaceBytes(const char* filename, const char* searchPattern, const char* replaceBytes, size_t replaceLength);
 	bool PatchClient();
 	bool PatchServer();
-	void CreateOriginalBackup();
+	void BackupFiles();
 	void CleanPatchFiles();
 }
