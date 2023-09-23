986107a7de0ee8fb9af17704d799161f27d79043
karl-police <karl-police2001@bluewin.ch>
2023-09-23 16:26:46 +0200

    Update main.cpp

--- CS2Downloader/main.cpp
+++ CS2Downloader/main.cpp
@@ -7,27 +7,54 @@
 #include "downloader.hpp"
 #include "CS2Patch.hpp"
 
-bool b_PatchOnly = false;
-
 std::string wantsMovementPatch;
 
 
-void patch_files() {
-	puts("Starting Patches...");
-	Patcher::PatchClient();
-	puts("Do you want to install the Patch?");
-	puts("Press 'Y' for 'Yes' or 'N' for 'No' on your keyboard");
-	std::cin >> wantsMovementPatch;
-
-	for (char& c : wantsMovementPatch) { 
-		c = std::tolower(c);
+namespace Operations {
+	void download() {
+		puts("Preparing Download..");
+		Downloader::PrepareDownload();
+		puts("Prepared Download!");
+		
+		Patcher::CleanPatchFiles();
+		puts("Starting Download...");
+		Downloader::DownloadCS2();
+		puts("Finished Download!.");
 	}
 
-	if (wantsMovementPatch.find("y") != std::string::npos) {
-		Patcher::PatchServer();
+	void GamePatches() {
+		puts("Starting GamePatches...");
+		puts("This can take a long time...");
+		Downloader::DownloadMods();
+		puts("Finished GamePatches!");
 	}
 
-	puts("Finished Patches!");
+	void cleanManifests() {
+		puts("Cleaning up downloader files...");
+		if (!Globals::usesNoManifests) {
+			std::filesystem::remove_all("manifestFiles");
+		}
+		puts("Cleaned up!");
+		puts("Download complete!");
+	}
+
+	void patch_files() {
+		puts("Starting Patches...");
+		Patcher::PatchClient();
+		puts("Do you want to install the Patch?");
+		puts("Press 'Y' for 'Yes' or 'N' for 'No' on your keyboard");
+		std::cin >> wantsMovementPatch;
+	
+		for (char& c : wantsMovementPatch) { 
+			c = std::tolower(c);
+		}
+	
+		if (wantsMovementPatch.find("y") != std::string::npos) {
+			Patcher::PatchServer();
+		}
+	
+		puts("Finished Patches!");
+	}
 }
 
 
@@ -51,39 +78,38 @@ int main(int argc, char* argv[]) {
 		if ( getArgFlag(argv[i], "disablemanifest") ) {
 			Globals::usesNoManifests = true;
 		}
-
-		if ( getArgFlag(argv[i], "patch_only") ) {
-			b_PatchOnly = true;
-		}
 	}
 
+
+	std::string m_inputOperation;
 	
 	puts("Daived: Thx for use my app");
 
-	if (b_PatchOnly == false) {
-		puts("Preparing Download..");
-		Downloader::PrepareDownload();
-		puts("Prepared Download!");
-		
-		Patcher::CleanPatchFiles();
-		puts("Starting Download...");
-		Downloader::DownloadCS2();
-		puts("Finished Download!.");
-	}
+	puts("What action do you want to perform?");
+	puts("download, patch");
+	std::cin >> m_inputOperation;
 
-	// Patch files
-	patch_files();
-	
-	puts("Starting GamePatches...");
-	puts("This can take a long time...");
-	Downloader::DownloadMods();
-	puts("Finished GamePatches!");
-	puts("Cleaning up downloader files...");
-	if (!Globals::usesNoManifests) {
-		std::filesystem::remove_all("manifestFiles");
+	if (m_inputOperation == "download") {
+		Operations::download();
+	else if (m_inputOperation == "patch") {
+		// Patch files
+		Operations::patch_files();
+
+		std::string m_doGamePatches;
+		
+		puts("Do GamePatches (mods), Y or N");
+		std::cin >> m_doGamePatches;
+
+		m_doGamePatches = std::tolower(m_doGamePatches);
+
+		if (m_doGamePatches.find("y") != std::string::npos) {
+			Operations::GamePatches();
+		}
 	}
-	puts("Cleaned up!");
-	puts("Download complete!");
+	
+	// Clean up
+	Operations::cleanManifests();
+	
 	puts("Daived: Thanks for use my app, Press 'Enter' to close the installer!");
 	waitforinput();
 	return 0;
